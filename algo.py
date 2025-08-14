f8feba266843f012f8414154cd5d593ee8fc5c37
Onyechere <ponyechere@bloomberg.com>
2025-08-14 09:10:08 +0100

    Final

---
+++ algo.py
@@ -0,0 +1,10 @@
+#How does this game work? lets understand how to sudoku in real scenario
+#Lets create the game board 
+#- We will need 3 rows and 3 columns
+#-We will need two players
+#-We will need each player to represent X and another to represent Y
+
+#Lets create the empty cells in the board
+#- We will neeed  our knowlege of loops
+
+#Show the Board after creating
